363ef24e35faaaf9a12bd2fc5e4df52dc5638efa
Muchamad Galih Anggara <muchamad.ga@ad-ins.com>
2020-12-09 12:27:54 +0700

    * Change null datatable value to user email

--- app/DataTables/SkpiDataTable.php
+++ app/DataTables/SkpiDataTable.php
@@ -23,7 +23,7 @@ class SkpiDataTable extends DataTable
         return datatables()
             ->eloquent($query)
             ->addColumn("nim", function ($skpi) {
-                return $skpi->user->kalbiser->nim ?? "-";
+                return $skpi->user->kalbiser->nim ?? $skpi->user->email;
             })
             ->addColumn("nama", function ($skpi) {
                 return $skpi->user->kalbiser->nama ?? "-";
