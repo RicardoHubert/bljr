fd2ea149bbbfd0ea412c105609e9ec2c7e5e16d6
galihanggara68 <galihanggara68@gmail.com>
2021-01-13 23:08:54 +0700

    * .gitignore on public

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(() => {
+    $(".btn-approval").on("click", e => {
+        let id = $(e.target).data("id");
+        let status = parseInt($(e.target).data("status"));
+        let spiner = $(e.target)
+            .parent()
+            .find(".spiner");
+        $(spiner).removeClass("hidden");
+
+        $.ajax({
+            url: `${BASE_URL}/ajax/skpi/approval`,
+            type: "PUT",
+            data: {
+                status: status == 1 ? 0 : 1,
+                id: id,
+                _token: $('meta[name="csrf-token"]').attr("content")
+            },
+            success: data => {
+                if (data.status == 1) {
+                    $(e.target).removeClass("btn-success");
+                    $(e.target).addClass("btn-warning");
+                    $(e.target)
+                        .find("span")
+                        .text("Disapprove");
+                } else {
+                    $(e.target).removeClass("btn-warning");
+                    $(e.target).addClass("btn-success");
+                    $(e.target)
+                        .find("span")
+                        .text("Approve");
+                }
+                $(e.target).data("status", data.status);
+                $(spiner).addClass("hidden");
+            }
+        });
+    });
+});
